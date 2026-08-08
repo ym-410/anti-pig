169c3e743cbd2e44640d53636f84bedc70815250
ym <225009064+ym-410@users.noreply.github.com>
2026-08-09 05:22:10 +0900

    fix: 機能ごとページ分け

--- app.py
+++ app.py
@@ -71,6 +71,22 @@ def get_total():
 
     return result[0] or 0
 
+def get_month_total(month):
+    connection = sqlite3.connect(DATABASE)
+
+    result = connection.execute(
+        """
+        SELECT SUM(amount)
+        FROM transactions
+        WHERE substr(date, 1, 7) = ?
+        """,
+        (month,),
+    ).fetchone()
+
+    connection.close()
+
+    return result[0] or 0
+
 create_table()
 
 @app.route("/", methods=["GET", "POST"])
@@ -78,14 +94,27 @@ def index():
     if request.method == "POST":
         return add_transaction()
 
+    month = request.args.get("month", "2026-08")
+
     transactions = get_transactions()
     total = get_total()
+    month_total = get_month_total(month)
 
     return render_template(
     "index.html",
     transactions = transactions,
     total = total,
+    month_total=month_total,
+    month=month,
 )
 
+@app.route("/calendar", methods=["GET"])
+def calender():
+    return render_template("calendar.html")
+
+@app.route("/graph", methods=["GET"])
+def graph():
+    return render_template("graph.html")
+
 if __name__ == "__main__":
-    app.run(debug=False) # コード変更時に再度読み込み
+    app.run(debug=True) # コード変更時に再度読み込み
