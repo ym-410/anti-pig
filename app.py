dc8f003adc4186c27c9afb05be536f8c4e368dc5
ym <225009064+ym-410@users.noreply.github.com>
2026-08-09 13:47:38 +0900

    カレンダーの収支表作成

--- app.py
+++ app.py
@@ -1,119 +1,47 @@
-import sqlite3
-from flask import Flask, redirect, render_template, request # 本体作成 / html読み込み
+import db_handle
+from flask import Flask, render_template, request # 本体作成 / html読み込み
 
 app = Flask(__name__)
 DATABASE = "antipig.db"
 
-def add_transaction():
-    amount_text = request.form["amount"]
-    category = request.form["category"]
-    date = request.form["date"]
-    memo = request.form["memo"]
-    amount = int(amount_text)
 
-    connection = sqlite3.connect(DATABASE)
+db_handle.create_table()
 
-    connection.execute(
-        """
-        INSERT INTO transactions (amount, category, date, memo)
-        VALUES (?, ?, ?, ?)
-        """,
-        (amount, category, date, memo),
-    )
-
-    connection.commit()
-    connection.close()
-
-    return redirect("/")
-
-def get_transactions():
-    connection = sqlite3.connect(DATABASE)
-    transactions = connection.execute(
-        """
-        SELECT id, amount, category, date, memo
-        FROM transactions
-        ORDER BY id DESC
-        """
-    ).fetchall()
-
-    connection.close()
-
-    return transactions
-
-
-
-
-def create_table():
-    connection = sqlite3.connect(DATABASE)
-
-    connection.execute(
-        """
-        CREATE TABLE IF NOT EXISTS transactions(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            amount INTEGER NOT NULL,
-            category TEXT NOT NULL,
-            date TEXT NOT NULL,
-            memo TEXT NOT NULL
-        )            
-        """
-    )
-
-    connection.close()
-
-def get_total():
-    connection = sqlite3.connect(DATABASE)
-
-    result = connection.execute(
-        "SELECT SUM(amount) FROM transactions"
-    ).fetchone()
-
-    connection.close()
-
-    return result[0] or 0
-
-def get_month_total(month):
-    connection = sqlite3.connect(DATABASE)
-
-    result = connection.execute(
-        """
-        SELECT SUM(amount)
-        FROM transactions
-        WHERE substr(date, 1, 7) = ?
-        """,
-        (month,),
-    ).fetchone()
-
-    connection.close()
-
-    return result[0] or 0
-
-create_table()
-
+# 入力ページ
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        return add_transaction()
+        return db_handle.add_transaction()
 
     return render_template(
     "index.html",
     )
 
+# カレンダーページ
 @app.route("/calendar", methods=["GET"])
 def calender():
     month = request.args.get("month", "2026-08")
+    is_income = request.args.get("is_income")
 
-    transactions = get_transactions()
-    total = get_total()
-    month_total = get_month_total(month)
-
+    transactions = db_handle.get_transactions()
+    total = db_handle.get_total()
+    month_record = db_handle.get_month_record(month)
+    income_total = db_handle.month_income(month)
+    expense_total = db_handle.month_expense(month)
+    month_total = income_total - expense_total
+    
     return render_template(
         "calendar.html",
         transactions = transactions,
         total = total,
-        month_total=month_total,
+        month_record=month_record,
         month=month,
+        month_total=month_total,
+        income_total=income_total,
+        expense_total=expense_total,
     )
 
+# グラフページ
 @app.route("/graph", methods=["GET"])
 def graph():
     return render_template("graph.html")
---
+++ db_handle.py
@@ -0,0 +1,149 @@
+import sqlite3
+from flask import redirect, request # 本体作成 / html読み込み
+
+
+DATABASE = "antipig.db"
+
+# テーブル作成
+def create_table():
+    connection = sqlite3.connect(DATABASE)
+
+    connection.execute(
+        """
+        CREATE TABLE IF NOT EXISTS transactions(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount INTEGER NOT NULL,
+            category TEXT NOT NULL,
+            date TEXT NOT NULL,
+            memo TEXT NOT NULL,
+            is_income INTEGER NOT NULL
+        )            
+        """
+    )
+
+    connection.close()
+
+# 追加
+def add_transaction():
+    amount_text = request.form["amount"]
+    category = request.form["category"]
+    is_income = request.form["is_income"]
+    date = request.form["date"]
+    memo = request.form["memo"]
+    amount = int(amount_text)
+
+    connection = sqlite3.connect(DATABASE)
+
+    connection.execute(
+        """
+        INSERT INTO transactions (amount, category, date, memo, is_income)
+        VALUES (?, ?, ?, ?, ?)
+        """,
+        (amount, category, date, memo, is_income),
+    )
+
+    connection.commit()
+    connection.close()
+
+    return redirect("/")
+
+# 読み取り
+def get_transactions():
+    connection = sqlite3.connect(DATABASE)
+    transactions = connection.execute(
+        """
+        SELECT id, amount, category, date, memo, is_income
+        FROM transactions
+        ORDER BY id DESC
+        """
+    ).fetchall()
+
+    connection.close()
+
+    return transactions
+
+# 更新
+
+# 削除
+
+
+# 月のデータ取得
+def get_month_record(month):
+    connection = sqlite3.connect(DATABASE)
+    month_record = connection.execute(
+        """
+        SELECT id, amount, category, date, memo, is_income
+        FROM transactions
+        WHERE substr(date, 1, 7) = ?
+        ORDER BY id DESC
+        """,
+        (month,),
+    ).fetchall()
+
+    connection.close()
+
+    return month_record
+
+# 月の合計取得
+def month_total(month):
+    connection = sqlite3.connect(DATABASE)
+
+    month_total = connection.execute(
+        """
+        SELECT SUM(amount)
+        FROM transactions
+        WHERE substr(date, 1, 7) = ?
+        """,
+        (month,),
+    ).fetchone()
+
+    connection.close()
+
+    return month_total[0] or 0
+
+# 月の収入合計
+def month_income(month):
+    connection = sqlite3.connect(DATABASE)
+
+    month_income = connection.execute(
+        """ 
+        SELECT SUM(amount)
+        FROM transactions
+        WHERE substr(date, 1, 7) = ? AND is_income = 0
+        """,
+        (month,),
+    ).fetchone()
+
+    connection.close()
+
+    return month_income[0] or 0
+
+# 月の支出合計
+def month_expense(month):
+    connection = sqlite3.connect(DATABASE)
+
+    month_expense = connection.execute(
+        """
+        SELECT SUM(amount)
+            FROM transactions
+            WHERE substr(date, 1, 7) = ? and is_income = 1
+        """,
+        (month,),
+    ).fetchone()
+
+    connection.close()
+
+    return month_expense[0] or 0
+
+
+# 合計取得
+def get_total():
+    connection = sqlite3.connect(DATABASE)
+
+    result = connection.execute(
+        "SELECT SUM(amount) FROM transactions"
+    ).fetchone()
+
+    connection.close()
+
+    return result[0] or 0
